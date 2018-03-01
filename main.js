123a494f4fa5e0a4140ee27e0b91c168c7e7b14a
Adam Wieckert <wiec1369@gmail.com>
2018-03-01 17:44:30 -0600

    Added some comments/ideas for challenge 1

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("Index is Linked");
+
+// Need to create an Array out of the string
+// Splice the string at the "space" character
+
+// assign numerical values to each letter by having each word in it's own array
+// sum the numerical values
+// compare each array.
+// output the values to the DOM
+// .split(" ") may split each word into a seperate index of an array. can then put those indexes into seperate arrays
+
+var mystring = "aaA"
+var newString = mystring.charCodeAt(2);
+console.log(newString);
