bd91e09fbd8b470f2e370c41996db78f9e014ed4
Adam Wieckert <wiec1369@gmail.com>
2018-03-01 19:25:25 -0600

    Added JS that console logs each words score successfully-----Note that this is hard coded, not in function string is defined and can only be 2 words long. Would like to make it dynamic and inside a function.

--- main.js
+++ main.js
@@ -9,6 +9,40 @@ console.log("Index is Linked");
 // output the values to the DOM
 // .split(" ") may split each word into a seperate index of an array. can then put those indexes into seperate arrays
 
-var mystring = "aaA"
-var newString = mystring.charCodeAt(2);
-console.log(newString);
+var letterNumbers = function (value) {
+
+}
+
+var string = "jump high";
+var word1
+var word2
+var word1Score = 0;
+var word2Score = 0;
+
+
+
+var array = string.split(' ');
+console.log(array);
+word1 = array[0];
+word2 = array[1];
+console.log("word vlaues: ", word1, word2)
+console.log(word1[0]);
+
+
+for(var i = 0; i < word1.length; i++){
+    var wordCode = word1.charCodeAt(i);
+    var letterNumber = wordCode - 96;
+    word1Score += letterNumber;
+}
+
+console.log("word1Score: ", word1Score);
+
+for(var i = 0; i < word2.length; i++){
+    var wordCode = word2.charCodeAt(i);
+    var letterNumber = wordCode - 96;
+    word2Score += letterNumber;
+}
+
+console.log("word2Score: ", word2Score);
+
+
