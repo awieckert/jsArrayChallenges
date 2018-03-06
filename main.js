e2be8c2fe4c634c1406e36f05cee91a56e390996
Adam Wieckert <wiec1369@gmail.com>
2018-03-05 19:47:09 -0600

    Made a function that finds the missing number in an array

--- main.js
+++ main.js
@@ -12,6 +12,11 @@ console.log("Index is Linked");
 // .split(" ") may split each word into a seperate index of an array. can then put those indexes into seperate arrays
 
 
+// Challenge-1 Advance instruction from Lauren
+
+// Challenge-1 Advance instruction END-----!!!
+
+
 var letterNumbers = function (value) {
 
     var array = value.split(' ');
@@ -66,3 +71,28 @@ console.log(lastIndexOf3);
 
 
 
+// Challenge-6 START------!
+
+var test1 = [1,3]; // 2
+var test2 = [2,3,4]; // 1
+var test3 = [13,11,10,3,2,1,4,5,6,9,7,8]; // 12
+var test4 = [1,2,3,4]; // 5
+
+function mathStuffs(testArray){
+    var correctArrayLength = (testArray.length) + 1;
+    var currectSum = (correctArrayLength * (correctArrayLength + 1)) / 2;
+   
+    var testArraySum = 0;
+    for(var i = 0; i < testArray.length; i++){
+        testArraySum += testArray[i];
+    }
+
+    var missingNumber = currectSum - testArraySum;
+    var printDiv = document.getElementById('challenge-6');
+    printDiv.innerHTML += "<h1>The missing number is: " + missingNumber + "</h1>"
+}
+
+mathStuffs(test1);
+mathStuffs(test2);
+mathStuffs(test3);
+mathStuffs(test4);
